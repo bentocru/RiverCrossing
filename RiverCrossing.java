5a0ff7eeedf800812689f8673a9b411dbfcc3e5c
Bento Cruz <bentocru@lancs.local>
2017-05-22 22:02:20 +0100

    puzzle with images and stump

--- RiverCrossing.java
+++ RiverCrossing.java
@@ -1,33 +1,60 @@
 import java.awt.Container;
 import java.awt.GridLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
-public class RiverCrossing extends JFrame {
+public class RiverCrossing implements ActionListener {
+    JButton[][] button = new JButton[9][13];
+    ImageIcon water  = new ImageIcon("C:/Users/bentocru/IdeaProjects/RiverCrossing/src/water1.jpg");
+    ImageIcon bank1  = new ImageIcon("C:/Users/bentocru/IdeaProjects/RiverCrossing/src/bank1.jpg");
+    ImageIcon bank2  = new ImageIcon("C:/Users/bentocru/IdeaProjects/RiverCrossing/src/bank2.jpg");
+	ImageIcon stump2_man  = new ImageIcon("C:/Users/bentocru/IdeaProjects/RiverCrossing/src/stump2_man.jpg");
 
-	public static void main(String[] args){
-		int rows = 9;
-		int cols = 13;
-		RiverCrossing gt = new RiverCrossing(rows, cols);
-		gt.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		gt.pack();
-		gt.setVisible(true);
-	}
+    JFrame mainFrame = new JFrame();
+    JPanel UIPanel = new JPanel();
+    JPanel gamePanel = new JPanel();
 	
-	public RiverCrossing(int rows, int cols) {
-		 Container pane = getContentPane();
-		 pane.setLayout(new GridLayout(rows, cols));
-		 for (int i = 0; i < 117; i++){
-			JButton button = new JButton(Integer.toString(i +1));
-			pane.add(button);
-		 }
-		 
-	JButton button = new JButton();
-	try {
-		Image img = ImageIO.read(getClass().getResource("resources/bank1.jpg"));
-		button.setIcon(new ImageIcon(img));}
-		catch (Exception ex) {
-		System.out.println(ex);
-		}		 
-	}
+
+    public void RiverCrossing() {
+        gamePanel.setLayout(new GridLayout(13,9));
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        for(int x=0; x<9; x++){
+            for(int i= 0; i<13; i++){
+                button[x][i] = new JButton(water);
+                gamePanel.add(button[x][i]);
+            }
+        }
+
+        for(int x =0; x<1; x++) {
+            for (int i = 0; i < 9; i++) {
+                button[x][i].setIcon(bank2);
+            }
+        }
+
+        for(int x =8; x<9; x++) {
+            for (int i = 4; i < 13; i++) {
+                button[x][i].setIcon(bank1);
+            }
+        }
+
+        //GETS RID OF LINESZZZZ
+        for(int x =0; x<9; x++) {
+            for (int i = 0; i < 13; i++) {
+                button[x][i].setBorderPainted(false);
+                button[x][i].setFocusPainted(false);
+            }
+        }
+		
+		button[8][6].setIcon(stump2_man);
+	
+        mainFrame.add(UIPanel);
+        mainFrame.setSize(310,350);
+        mainFrame.add(gamePanel);
+        mainFrame.setVisible(true);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
---
+++ main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by bentocru on 22/05/2017.
+ */
+public class main {
+    public static void main(String[] args){
+        RiverCrossing riverCrossing = new RiverCrossing();
+        riverCrossing.RiverCrossing();
+    }
+}
